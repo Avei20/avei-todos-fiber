421f2a96687f6798adeaff9aeb1c1e4fdb9019d4
muhammadabdulazizalghofari <muhammadabdulazizalghofari@gmail.com>
2024-06-23 00:59:21 +0000

    Adjusting server.go

--- internal/app/server.go
+++ internal/app/server.go
@@ -42,9 +42,9 @@ func (s *Server) InitRouteAndServe() {
 	app.Mount("/", router.InitRouter(&s.handlers))
 
 	// Swagger
-	app.Get("/swagger/*", swagger.HandlerDefault) // default
+	app.Get("/docs/*", swagger.HandlerDefault) // default
 
-	app.Get("/swagger/*", swagger.New(swagger.Config{ // custom
+	app.Get("/docs/*", swagger.New(swagger.Config{ // custom
 		URL:         "http://example.com/doc.json",
 		DeepLinking: false,
 		// Expand ("list") or Collapse ("none") tag groups by default
@@ -54,6 +54,7 @@ func (s *Server) InitRouteAndServe() {
 			AppName:  "OAuth Provider",
 			ClientId: "21bb4edc-05a7-4afc-86f1-2e151e4ba6e2",
 		},
+		
 		// Ability to change OAuth2 redirect uri location
 		OAuth2RedirectUrl: "http://localhost:8080/swagger/oauth2-redirect.html",
 	}))
