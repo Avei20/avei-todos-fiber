e98a771e77ad4136b51b96a9ad451d9d6ed17e15
Avei20 <muhammadabdulazizalghofari@gmail.com>
2024-07-19 18:00:56 +0700

    feat: Adding CORS Support

--- internal/app/server.go
+++ internal/app/server.go
@@ -11,6 +11,7 @@ import (
 	_ "avei-todos-fiber/docs"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/swagger"
 )
 
@@ -52,9 +53,14 @@ func (s *Server) InitRouteAndServe() {
 			AppName:  "OAuth Provider",
 			ClientId: "21bb4edc-05a7-4afc-86f1-2e151e4ba6e2",
 		},
-		
+
 		// Ability to change OAuth2 redirect uri location
 		OAuth2RedirectUrl: "http://localhost:8080/swagger/oauth2-redirect.html",
+	}))
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
 	port := os.Getenv("PORT")
